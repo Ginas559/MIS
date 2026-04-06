406fea9dd84723848aa28f65bd1f578c2a16d0da
Ginas559 <levanphong511@gmail.com>
2026-04-06 20:07:41 +0700

    LCMS-16 feat: implement person hierarchy and factory pattern

---
+++ src/main/java/vn/iotstar/coolenglish/factory/StudentRegistration.java
@@ -0,0 +1,21 @@
+package vn.iotstar.coolenglish.factory;
+
+import vn.iotstar.coolenglish.entity.Person;
+import vn.iotstar.coolenglish.entity.Student;
+import vn.iotstar.coolenglish.enums.StudentStatus;
+
+public class StudentRegistration extends UserRegistration {
+
+    @Override
+    protected Person createPerson() {
+        return new Student();
+    }
+
+    @Override
+    protected void initializeSpecificInfo(Person person) {
+        Student student = (Student) person;
+        student.setStudentID("STU-" + System.currentTimeMillis());
+        student.setStatus(StudentStatus.ACTIVE);
+    }
+}
+
---
+++ src/main/java/vn/iotstar/coolenglish/factory/TeacherRegistration.java
@@ -0,0 +1,21 @@
+package vn.iotstar.coolenglish.factory;
+
+import vn.iotstar.coolenglish.entity.Person;
+import vn.iotstar.coolenglish.entity.Teacher;
+import vn.iotstar.coolenglish.enums.TeacherStatus;
+
+public class TeacherRegistration extends UserRegistration {
+
+    @Override
+    protected Person createPerson() {
+        return new Teacher();
+    }
+
+    @Override
+    protected void initializeSpecificInfo(Person person) {
+        Teacher teacher = (Teacher) person;
+        teacher.setTeacherID("TCH-" + System.currentTimeMillis());
+        teacher.setStatus(TeacherStatus.ACTIVE);
+    }
+}
+
